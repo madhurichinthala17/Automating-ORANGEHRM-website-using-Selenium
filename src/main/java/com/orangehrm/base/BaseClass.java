b97591105ad405eb45f6370a8e2a5bf11ce0148a
madhurichinthala17 <madhurichinthala2000@gmail.com>
2024-07-02 21:44:04 -0400

    searchingforanemployee

--- src/main/java/com/orangehrm/base/BaseClass.java
+++ src/main/java/com/orangehrm/base/BaseClass.java
@@ -2,10 +2,14 @@ package com.orangehrm.base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
@@ -79,7 +83,7 @@ public class BaseClass {
         ///logout();
     }
 
-    @Test(priority=2)
+    /*@Test(priority=2)
     public void addEmployee()
     {
         driver.findElement(By.xpath("//a[text()='Employee List ']")).click();
@@ -88,17 +92,58 @@ public class BaseClass {
         driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(prop.getProperty("FirstName"));
         driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(prop.getProperty("LastName"));
 
-        //driver.findElement(By.xpath("//div[@class='input-group-append']//i[contains(text(),'arrow_drop_down')]")).click();
-        driver.findElement(By.xpath("//input[@value='Canadian Development Center']")).click();
+        driver.findElement(By.xpath("//div[@class='input-group-append']//i[contains(text(),'arrow_drop_down')]")).click();
+        //driver.findElement(By.xpath("//input[@value='Canadian Development Center']")).click();
+        //WebElement dropdownButton = driver.findElement(By.xpath("//button[@value='-- Select --']"));
+        //dropdownButton.click();
+        // Use JavaScript Executor to find and click the option since it's dynamically loaded
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        // Locate the desired option using JavaScript and click on it
+        WebElement dropdownOption = (WebElement) js.executeScript(
+                "return document.evaluate(\"//div[@class='input-group-append-container']//button[contains(text(), 'Australia office')]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;");
+
+        // Click the option
+        js.executeScript("arguments[0].click();", dropdownOption);
+
         driver.findElement(By.xpath("//button[text()='Next']")).click();
 
 
 
 
 
+    }*/
+
+    @Test(priority = 2)
+    public void searchEmployeeByName()
+    {
+        driver.findElement(By.xpath("//a[text()='Employee List ']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        driver.findElement(By.xpath("//input[@placeholder='Employee Name']")).sendKeys("Madhuri");
+        driver.findElement(By.xpath("//i[@id='quick_search_icon']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Wait for the toast message to appear and capture its text
+        String expectedMessage = "No Records Found";
+        try {
+            WebElement toastMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='toast-message']")));
+            String actualMessage = toastMessage.getText();
+
+            // Verify the message
+            if (actualMessage.equals(expectedMessage)) {
+                System.out.println("No RECORDS");
+            } else {
+                System.out.println("RECORD FOUND");
+            }
+        } catch (Exception e) {
+            System.out.println("RECORD FOUND");
+        }
+
+
     }
 
-    @Test(priority = 3)
+    @Test(priority = 3,enabled = false)
     public void loginTestWithInvalidCredentials()
     {
         driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys(prop.getProperty("username"));
